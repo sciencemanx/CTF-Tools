7f0c7b32045b3b4f548f3a8ecf92dbef785f6f53
Adam Van Prooyen <adam.vanpr@gmail.com>
2016-04-06 15:58:22 -0400

    Removed unnecessary Ace Editor files from repo. Moved external javascript packages to Bower. Added requirements.txt for python dependencies.

--- scheduler/launcher.py
+++ scheduler/launcher.py
@@ -7,16 +7,19 @@ import scheduler
 exploitdir = 'exploits'
 path = os.path.join(os.getcwd(), exploitdir)
 
-def auto_load(interval=5):
-	threading.Timer(interval, auto_load).start()
+def auto_load(interval=240):
+	threading.Timer(interval, auto_load, args=(interval,)).start()
 	for f in os.listdir(path):
+		if f == ".DS_Store": 
+			continue
 		module = '{}.{}'.format(exploitdir, f.split('.')[0])
 		importlib.import_module(module)
 
 	with open('conf.json', 'r') as f:
 		conf = json.loads(f.read())
+	print('ips: {}'.format(conf["ips"]))
 	scheduler.ips = conf["ips"]
 	scheduler.omitted = conf["omitted"]
 
-auto_load()
+auto_load(240)
 scheduler.launch()
--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,6 +1,8 @@
 import requests as r
 import threading
 from functools import wraps
+import os
+import json
 
 _tasks = []
 ips = []
@@ -18,22 +20,26 @@ def schedule(func):
 		flag = func(ip)
 		if flag:
 			info('task {} retreived flag: {} from ip: {}'.format(func.__qualname__, flag, ip))
-			success = submit(flag)
+			success, message = submit(flag)
 			if success:
 				info('task {} successfully submitted flag'.format(func.__qualname__))
 			else:
-				warn('task {} failed to submit flag {}'.format(func.__qualname__, flag))
+				warn('task {} failed to submit flag {} because {}'.format(func.__qualname__, flag, message))
 		else:
 			warn('task {} failed to retreive flag from ip: {}'.format(func.__qualname__, ip))
 	_tasks.append(task_func)
 	return task_func
 
 def submit(flag):
-	# submit logic here (probably a post request)
-	pass
+	res = r.post('https://live.cyberstakesonline.com/liveapi/2/submit',
+				  {'apikey': '8abb3ebb021c734590d41c42afd498854d848644f8cc38e11d',
+				   'flag': flag})
+	res = json.loads(res.text)
+	return (res['status'] == '1', res['message'])
+	
 
-def launch(interval=5.0):
-	threading.Timer(interval, launch).start()
+def launch(interval=240):
+	threading.Timer(interval, launch, args=(interval,)).start()
 	for task in _tasks:
 		if task.__qualname__ in omitted:
 			continue
